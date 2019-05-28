a3fc8cacaba5781fb6a5e168647fbf228e781bd5
nicolasteixeira3856 <nicolasteixeira3856@outlook.com>
2019-05-28 10:40:24 -0300

    Adicionando tipos de requisições: get, post, put, delete

--- index.php
+++ index.php
@@ -1,9 +1,63 @@
-<?php
+<?php 
+    use Psr\Http\Message\ServerRequestInterface as Request;
+    use Psr\Http\Message\ResponseInterface as Response;
 
     require 'vendor/autoload.php';
 
     $app  = new \Slim\App;
 
+    //Padrão PSR7
+    $app->get('/postagens', function(Request $request, Response $response){
+        //Esreve no corpo da resposta utilizando o padrão PSR7
+        $response->getBody()->write("Listagem de postagens");
+        return $response;
+    });
+
+    /* Tipos de requisição ou verbos HTTTP 
+
+    get -> Recuperar recursos do servidor (select)
+    post -> Criar dado no servidor (insert)
+    put -> Atualizar dados no servidor (update)
+    delete -> Deletar dados no servidor (delete)
+
+    */
+
+    $app->post('/usuarios/adiciona', function(Request $request, Response $response){
+        //Esreve no corpo da resposta utilizando o padrão PSR7
+        //$_POST
+        $post   = $request->getParsedBody();
+        $nome   = $post['Nome'];
+        $email  = $post['Email'];
+        /* Salvar no banco de dados */
+        //return $response->getBody()->write("Sucesso");
+        return $response->getBody()->write($nome. " - ".$email);
+    });
+
+    $app->put('/usuarios/atualiza', function(Request $request, Response $response){
+        //Esreve no corpo da resposta utilizando o padrão PSR7
+        //$_POST
+        $post   = $request->getParsedBody();
+        $id     = $post['id'];
+        $nome   = $post['nome'];
+        $email  = $post['email'];
+        /* Atualizar no banco de dados */
+        //return $response->getBody()->write("Sucesso");
+        return $response->getBody()->write("Sucesso ao atualizar: ". $id);
+    });
+
+    $app->delete('/usuarios/remove/{id}', function(Request $request, Response $response){
+        //Esreve no corpo da resposta utilizando o padrão PSR7
+        //$_POST
+        $id = $request->getAttribute('id');
+        /* Deletar no banco de dados */
+        //return $response->getBody()->write("Sucesso");
+        return $response->getBody()->write("Sucesso ao deletar: ". $id);
+    });
+
+
+    $app->run();
+
+    /*
     $app->get('/postagens', function(){
         echo "Listagem de Postagens";
     });
@@ -24,8 +78,9 @@
         var_dump(explode("/", $itens));
         echo $itens;
     });
+    */
 
-    /*Nomear Rotas */
+    /*Nomear Rotas
     $app->get('/blog/postagens/{id}', function($request, $response){
         echo 'Listar postagem para um ID';
     })->setName("blog");
@@ -34,9 +89,9 @@
         $retorno = $this->get("router")->pathFor("blog", ["id" => "5"]);
 
         echo $retorno;
-    });
+    });*/
 
-    /* Agrupar rotas */
+    /* Agrupar rotas
     $app->group('/v1', function(){
         $this->get('/postagens', function(){
             echo "Listagem de Postagens";
@@ -47,7 +102,11 @@
         });
     
     });
+    */
+
+
 
 
 
     $app->run();
+?>
